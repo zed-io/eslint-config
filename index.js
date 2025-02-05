6c984b0c12580540fb620c97eb0b6da9812f658a
shottah <wasteofintel@gmail.com>
2025-02-05 16:34:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  plugins: ['unused-imports', 'import'],
+  rules: {
+    'no-unused-vars': 'off', // Turned off in favor of unused-imports rule
+    "@typescript-eslint/no-unused-vars": "off", // Replaced by unused-imports
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': "error",
+    'import/order': "error"
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+}; 
